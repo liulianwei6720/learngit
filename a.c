0ca89f14403493d8a85c25d8c69945cc16e1f280
Liang_Yimiao <liulianwei6720@gmail.com>
2022-05-10 17:29:18 +0800

    write the power func

--- a.c
+++ a.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+
+int power(int a, int b)
+{
+    int p = 1;
+    for(int i = 1; i <= b; i++)
+        p *= a;
+    return p;
+}
+
 int main(void)
 {
     int a;
     a = 8;
-    printf("%d", a);
+    printf("%d\n", power(a, 3));
     printf("Hello World!\n");
     return 0;
 }
