5852dd263bd19e96b56608cf4233ac5ed0539600
Liang_Yimiao <liulianwei6720@gmail.com>
2022-05-10 17:37:55 +0800

    sys

--- a.c
+++ a.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int power(int a, int b)
 {
     int p = 1;
     for(int i = 1; i <= b; i++)
         p *= a;
+    printf("Hello World from power\n");
     return p;
 }
 
@@ -14,5 +15,6 @@ int main(void)
     a = 8;
     printf("%d\n", power(a, 3));
     printf("Hello World!\n");
+    system("pause");
     return 0;
 }
